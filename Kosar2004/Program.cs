b400af9e540bc3ad93c968e4292b0136158a7eec
varsanyib <varsanyib0125@gmail.com>
2022-10-13 11:43:43 +0200

    Add project files.

---
+++ Kosar2004/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kosar2004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
