01b10ddd0aa4cb1b45a47cd71d3a7e2ef86263dd
varsanyib <varsanyib0125@gmail.com>
2022-10-13 11:59:49 +0200

    Állapotmentés kész osztályról, illetve a harmadik feladatról.

--- Kosar2004/Program.cs
+++ Kosar2004/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Kosar2004.SajatOsztaly;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,45 @@ namespace Kosar2004
 {
     class Program
     {
+        static List<Merkozes> merkozesek = new List<Merkozes>();
         static void Main(string[] args)
         {
+            MasodikFeladat();
+            HarmadikFeladat("Real Madrid");
+
+            Console.ReadKey();
+        }
+
+        private static void HarmadikFeladat(string keresettCsapat)
+        {
+            Console.Write($"3. feladat: {keresettCsapat}: ");
+            int hazai = 0;
+            int vendeg = 0;
+            foreach (var x in merkozesek)
+            {
+                if (x.Hazai == keresettCsapat)
+                {
+                    hazai++;
+                }
+                if (x.Vendeg == keresettCsapat)
+                {
+                    vendeg++;
+                }
+            }
+            Console.WriteLine($"Hazai: {hazai}, Idegen: {vendeg}");
+        }
+
+        private static void MasodikFeladat()
+        {
+            using (StreamReader sr = new StreamReader("Adatfajl/eredmenyek.csv"))
+            {
+                sr.ReadLine(); //első sor skip
+                while (!sr.EndOfStream)
+                {
+                    merkozesek.Add(new Merkozes(sr.ReadLine()));
+                }
+
+            }
         }
     }
 }
---
+++ Kosar2004/SajatOsztaly/Merkozes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kosar2004.SajatOsztaly
+{
+    class Merkozes
+    {
+        private string hazai;
+
+        public string Hazai
+        {
+            get { return hazai; }
+            private set { hazai = value; }
+        }
+
+        private string vendeg;
+
+        public string Vendeg
+        {
+            get { return vendeg; }
+            private set { vendeg = value; }
+        }
+        private int hazaiPont;
+
+        public int HazaiPont
+        {
+            get { return hazaiPont; }
+            private set { hazaiPont = value; }
+        }
+        private int vendegPont;
+
+        public int VendegPont
+        {
+            get { return vendegPont; }
+            private set { vendegPont = value; }
+        }
+        private string helyszin;
+
+        public string Helyszin
+        {
+            get { return helyszin; }
+            private set { helyszin = value; }
+        }
+
+        private DateTime idopont;
+
+        public DateTime Idopont
+        {
+            get { return idopont; }
+            private set { idopont = value; }
+        }
+
+        public Merkozes(string sor)
+        {
+            string[] tmp = sor.Split(';');
+            Hazai = tmp[0];
+            Vendeg = tmp[1];
+            HazaiPont = Convert.ToInt32(tmp[2]);
+            VendegPont = Convert.ToInt32(tmp[3]);
+            Helyszin = tmp[4];
+            Idopont = Convert.ToDateTime(tmp[5]);
+        }
+    }
+}
